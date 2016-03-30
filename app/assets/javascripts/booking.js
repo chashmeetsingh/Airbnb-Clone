c702d63a49a8a4917e0283b35e3089b581d0452f
Jorge Barnaby <jorge.barnaby@gmail.com>
2016-03-30 11:31:20 -0400

    Minor fix

--- app/assets/javascripts/booking.js
+++ app/assets/javascripts/booking.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
   $selectPet = $('.select-pet');
 
-  $selectPet.click(function() {
+  $selectPet.click(function(e) {
     var $this = $(this);
     $selectPet.removeClass('active');
     $this.addClass('active');
     $('#dog_id').val($this.data('pet-id'));
+    e.preventDefault();
   });
 
   if ($('#bookButton').length) {
