600695eeea8c2cf9129858c26dcdfe96dea40614
Jorge Barnaby <jorge.barnaby@gmail.com>
2016-04-05 15:02:51 -0400

    Booking JS fix when new dog has been added

--- app/assets/javascripts/booking.js
+++ app/assets/javascripts/booking.js
@@ -1,11 +1,50 @@
 $(document).ready(function() {
-  $selectPet = $('.select-pet');
+  var $petList = $('#pet-list');
+  var $dogId = $('#dog_id');
 
-  $selectPet.click(function(e) {
-    var $this = $(this);
-    $selectPet.removeClass('active');
-    $this.addClass('active');
-    $('#dog_id').val($this.data('pet-id'));
+  $petList.click(function(e) {
+    var activeClass = 'active';
+    var $target = $(e.target);
+    $('.list-group-item', $petList).removeClass(activeClass);
+    $target.addClass(activeClass);
+    $dogId.val($target.data('pet-id'));
     e.preventDefault();
+  });
+
+  $('#dogForm').submit(function(e) {
+    e.preventDefault();
+
+    var $name = $('#name');
+    var $description = $('#description');
+
+    console.log('aaa');
+
+    if ($name.val()) {
+      if ($description.val()) {
+        $.post('/dog/api', {
+            name: $name.val(),
+            year: $('#year').val(),
+            month: $('#month').val(),
+            gender: $('#gender').val(),
+            breed: $('#breed').val(),
+            size: $('#size').val(),
+            description: $description.val()
+          },
+          function(data, status, xhr) {
+            if (xhr.status == 200) {
+              var content = '<a href class="list-group-item" data-pet-id="' + data['dog_id'] + '">' + data['dog_name'] + '</a>';
+              console.log(content);
+              $petList.append(content);
+              alert('added');
+            }
+            console.log(xhr.status);
+          }
+        )
+      } else {
+        alert('provide description');
+      }
+    } else {
+      alert('name empty');
+    }
   });
 });
