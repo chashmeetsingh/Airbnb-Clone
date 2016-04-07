b4589e9d0a48de339eee79841fea4cabeedb8195
Chashmeet Singh <chashmeetsingh@gmail.com>
2016-04-07 12:47:32 +0530

    added search by date

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -27,13 +27,15 @@ class HomeController < ApplicationController
   def search
     @lng = params[:lng].to_f
     @lat = params[:lat].to_f
+    @start_date = params[:start_date]
+    @end_date = params[:end_date]
 
     query = [ @lat, @lng ]
 
     @users = Array.new
     # @users = User.all
 
-    User.all.each do |user|
+    User.where(role: 0).each do |user|
 
       if user_signed_in?
         if user == current_user
@@ -41,11 +43,24 @@ class HomeController < ApplicationController
         end
       end
 
-      unless user.lat.nil? && user.lng.nil?
-        user_coor = [ user.lat, user.lng ]
+      boolBook = true
 
-        if Haversine.distance(query, user_coor).to_km < 20
-          @users << user
+      unless @start_date.empty? && @end_date.empty?
+        Booking.where(provider_id: user.id).each do |booking|
+          if @start_date.to_date <= booking.end_date && @end_date.to_date >= booking.start_date
+            boolBook = false
+          end
+          break if boolBook == false
+        end
+      end
+
+      if boolBook == true
+        unless user.lat.nil? && user.lng.nil?
+          user_coor = [ user.lat, user.lng ]
+
+          if Haversine.distance(query, user_coor).to_km < 20
+            @users << user
+          end
         end
       end
     end
